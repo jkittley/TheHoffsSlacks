c3a8324159dca6a84a456b6d7f0920580c7bd6d8
jkittley <jkd3g11@soton.ac.uk>
2016-01-22 13:35:46 +0000

    added twitter dependancies

--- hoff.js
+++ hoff.js
@@ -1,19 +1,19 @@
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello, ' + process.env.twitter_consumer_key + '!'
+    text : 'Hello, ' + userName + '!'
   };
 
   var guess = req.body.text;
 
   // Connect to twitter
-  // var Twitter = require('twitter');
-  // var client = new Twitter({
-  //   consumer_key: process.env.twitter_consumer_key,
-  //   consumer_secret: process.env.twitter_consumer_secret,
-  //   access_token_key: process.env.twitter_access_token_key,
-  //   access_token_secret: process.env.twitter_access_token_secret
-  // });
+  var Twitter = require('twitter');
+  var client = new Twitter({
+    consumer_key: process.env.twitter_consumer_key,
+    consumer_secret: process.env.twitter_consumer_secret,
+    access_token_key: process.env.twitter_access_token_key,
+    access_token_secret: process.env.twitter_access_token_secret
+  });
 
   // // Post message to twitter
   // var twitter_string = 'Slack user: ' + userName + ' suggested: '+ guess;
